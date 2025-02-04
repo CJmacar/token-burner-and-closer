3110717cdb71e3e1db7709c5d5eefb1af8da9156
CJmacar <mcarlen@gmail.com>
2025-02-05 00:22:49 +0100

    Update index.ts

--- supabase/functions/get-token-metadata/index.ts
+++ supabase/functions/get-token-metadata/index.ts
@@ -2,15 +2,15 @@ import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Authorization, Content-Type, x-client-info, apikey, content-type', 
+  'Access-Control-Allow-Methods': 'GET', 'POST, OPTIONS',
 }
 
 serve(async (req) => {
   // Handle CORS preflight requests
   if (req.method === 'OPTIONS') {
     console.log('Handling CORS preflight request')
-    return new Response(null, { headers: corsHeaders })
+    return new Response(null, { status: 204, headers: corsHeaders })
   }
 
   try {
